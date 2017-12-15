d57547edc02db5861c3cc84b107667f76d0cade3
ChinaGc <462846497@qq.com>
2017-12-15 09:38:00 +0800

    1

--- PushServer/src/main/java/roc/gc/push/core/enums/ErrorCode.java
+++ PushServer/src/main/java/roc/gc/push/core/enums/ErrorCode.java
@@ -2,7 +2,7 @@ package roc.gc.push.core.enums;
 
 public enum ErrorCode {
 
-    USER_NOT_EXIST(400, "用户不存在"), LOGIN_ON_ONTHER(401, "用户已在其他设备上登录"), SERVER_ERROR(500, "服务器错误");
+    USER_NOT_EXIST(404, "用户不存在"), LOGIN_ON_ONTHER(401, "用户已在其他设备上登录"), SERVER_ERROR(500, "服务器错误");
     public int code;
 
     public String desc;
